a84cd3b5d374cd4a092d3bf04b2e4477da9bfafb
Hardikkansara10 <22004500210086@ljku.edu.in>
2025-06-25 16:28:10 +0530

    rerote changes

--- tests/math_test.py
+++ tests/math_test.py
@@ -1,8 +1,8 @@
-from src.math_app import add,sub
+from src.math_app import add, sub
 
 def test_add():
     assert add(2,3)==5
-    assert add(-1,1)==5
+    assert add(-1,1)==0
 
 def test_sub():
     assert sub(3,2)==1
